df9b1097574eea7ecb96e4b980bdbeec35a379d9
mqWork <mqithelp@gmail.com>
2024-10-23 11:39:04 +0300

    init commit

---
+++ src/main/java/pro/mqithelp/questionsexam/QuestionsExamApplication.java
@@ -0,0 +1,13 @@
+package pro.mqithelp.questionsexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionsExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/mqithelp/questionsexam/QuestionsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.mqithelp.questionsexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
